1683e443e59f79f396dcbd3241a4740fd9ee9c12
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-02 00:57:40 +0530

    Add files via upload

---
+++ floyd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n = 1;
+    int rows;
+
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+
+        for (int j = 1; j <= i; j++) {
+
+            cout << n << " ";
+
+            n++;
+
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+
+}
