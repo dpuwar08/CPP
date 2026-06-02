46efa90dbcd79031ee4e891047a003dcd351a8e1
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-02 21:40:56 +0530

    Add files via upload

---
+++ fun2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int Sum ( int a , int b) {
+
+    int Sum = a + b;
+
+    return Sum;
+}
+
+int main (){
+
+    cout << Sum( 10 , 15 ) << endl;
+
+    return 0;
+
+}
