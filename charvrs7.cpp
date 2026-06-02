1e18eb0912b9a88a020e57ac770024dfea3cc405
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-02 13:00:32 +0530

    Add files via upload

---
+++ charvrs7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    char a = 'A';
+    int n = 4;
+
+    for ( int i = 1; i <= n ; i++ ) {
+
+        for ( int j = 1; j <= i ; j++) {
+
+            cout << a << " ";
+
+                a++;
+
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+
+}
