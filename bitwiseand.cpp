40fa6da86d5798c5cd79dcf93f55d29914e71723
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-03 20:14:46 +0530

    Add files via upload

---
+++ bitwiseand.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int a , b;
+
+    cout << " Enter two numbers: " ;
+    cin >> a;
+    cin >> b;
+
+    cout << ( a & b ) << endl;
+
+    
+    return 0;
+
+}
