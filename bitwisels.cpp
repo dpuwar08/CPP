b8e77f1d6d3503929c855a664f4232f0afbef5aa
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-03 20:26:38 +0530

    Add files via upload

---
+++ bitwisels.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int a ;
+
+    cout << " Enter a number : ";
+
+    cin >> a;
+
+    cout << ( a << 1 ) << endl;
+
+    return 0;
+
+}
