91c160515efb9065f39220096be61a46be907f7a
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-08 09:58:11 +0530

    Add files via upload

---
+++ implicit.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    for ( char i = 'A' ; i <= 'Z' ; i++ ) {
+
+        int value =  i ;
+
+        cout << i << " ASCII value - " << value << endl;
+
+    }
+
+    for ( char i = 'a' ; i <= 'z' ; i++ ) {
+
+        int value =  i ;
+
+        cout << i << " ASCII value - " << value << endl;
+
+    }
+
+    return 0;
+
+}
