288d01693132b5d02cd8bcb76add2aa6f7612798
d_puwar_08 <242736805+dpuwar08@users.noreply.github.com>
2026-06-08 17:49:04 +0530

    Add files via upload

---
+++ logicgates.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << !(3 < 1) << endl; 
+
+    cout << ((3 < 1) || (3 < 5))<< endl;
+
+    cout << ((3 < 1) && (3<5)) << endl; 
+    
+    return 0;
+
+}
